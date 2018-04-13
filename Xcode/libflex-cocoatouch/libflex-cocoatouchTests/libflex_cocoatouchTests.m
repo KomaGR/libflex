ef8de2999287a83d1fc3c99c68bcda74c80546d6
Phillip Stanley-Marbell <phillip.stanleymarbell@gmail.com>
2018-04-13 17:52:01 +0100

    Xcode project for building on iOS

---
+++ Xcode/libflex-cocoatouch/libflex-cocoatouchTests/libflex_cocoatouchTests.m
@@ -0,0 +1,33 @@
+//
+//  libflex_cocoatouchTests.m
+//  libflex-cocoatouchTests
+//
+//  Created by pip on 6/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "libflex_cocoatouchTests.h"
+
+
+@implementation libflex_cocoatouchTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in libflex-cocoatouchTests");
+}
+
+@end
